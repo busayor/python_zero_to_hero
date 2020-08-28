1a949e27fe57226d1d1a75f1b8768c42033e1188
oluwabusayo folaranmi <busayor@gmail.com>
2020-08-28 01:10:08 +0100

    Update lst_item.py

--- lst_item.py
+++ lst_item.py
@@ -1,9 +1,20 @@
-def last_item():
-  my_lst = [1,3,4,5,7,8]
-  if(len(my_lst) == 0):
-    return "empty list"
-  else:
-    return my_lst[-1]
+# def last_item():
+#   my_lst = [1,3,4,5,7,8]
+#   if(len(my_lst) == 0):
+#     return "empty list"
+#   else:
+#     return my_lst[-1]
 
 
-print (last_item())
+# print (last_item())
+
+
+def last_item(myTxt):
+    if(len(myTxt) == 0):
+       return "empty string" 
+    else:
+        return myTxt[-1]
+
+
+show_last_item = last_item(input("enter text here-->"))
+print (show_last_item)
