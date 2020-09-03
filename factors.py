91449a7fd81d49e7fa5356ed3ec3a0b5f733da13
oluwabusayo folaranmi <busayor@gmail.com>
2020-09-04 00:09:11 +0100

    Find factors of num, in increasing order.
    Find factors of num, in increasing order.

---
+++ factors.py
@@ -0,0 +1,10 @@
+# This function computes the factor of the argument passed
+def print_factors(x):
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+num = int(input("enter a number -->"))
+
+print_factors(num)
